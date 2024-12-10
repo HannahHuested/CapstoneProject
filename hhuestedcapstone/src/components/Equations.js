9b9d89ea36ae61196fb2d8bae20e9e3fbbda9e38
HannahHuested <123486478+HannahHuested@users.noreply.github.com>
2024-12-10 17:18:32 -0500

    Updates

--- hhuestedcapstone/src/components/Equations.js
+++
@@ -1,7 +0,0 @@
-import React from 'react'
-
-export const Equations = () => {
-  return (
-    <div><h3>Equations</h3></div>
-  )
-}
--- hhuestedcapstone/src/components/Stats.js
+++
@@ -1,7 +0,0 @@
-import React from 'react'
-
-export const Stats = () => {
-  return (
-      <div><h3>Stats</h3></div>
-  )
-}
